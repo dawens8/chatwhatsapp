cc58cb06f8182d55ecb0f967e7a8d5625d784848
dawens8 <mydawens8@gmail.com>
2025-09-16 21:24:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,27 +5,19 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-let users = {}; // {number: socketId}
+let users = {}; // {number: {socketId, videoStreamId}}
 let messages = {}; // {number: [{from, text, time, type}]}
 
-// Serve index.html
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/dashboard', (req, res) => res.sendFile(__dirname + '/public/dashboard.html'));
 
-// Serve dashboard
-app.get('/dashboard', (req, res) => {
-  res.sendFile(__dirname + '/public/dashboard.html');
-});
-
-// Socket connections
 io.on('connection', socket => {
-  console.log('New connection:', socket.id);
+  console.log('New connection', socket.id);
 
   socket.on('join', number => {
-    users[number] = socket.id;
+    users[number] = {socketId: socket.id};
     socket.number = number;
-    io.emit('statusUpdate', users); // update dashboard
+    io.emit('statusUpdate', users);
   });
 
   socket.on('message', data => {
@@ -33,7 +25,7 @@ io.on('connection', socket => {
     const msg = {from: socket.number, to: data.to, text: data.text, time, type: data.type || 'text'};
     if(!messages[data.to]) messages[data.to]=[];
     messages[data.to].push(msg);
-    if(users[data.to]) io.to(users[data.to]).emit('message', msg);
+    if(users[data.to]) io.to(users[data.to].socketId).emit('message', msg);
     io.to(socket.id).emit('message', msg);
   });
 
@@ -42,20 +34,29 @@ io.on('connection', socket => {
     const msg = {from: socket.number, to: data.to, audio: data.audio, time, type:'voice'};
     if(!messages[data.to]) messages[data.to]=[];
     messages[data.to].push(msg);
-    if(users[data.to]) io.to(users[data.to]).emit('voice', msg);
+    if(users[data.to]) io.to(users[data.to].socketId).emit('voice', msg);
     io.to(socket.id).emit('voice', msg);
   });
 
+  // WebRTC signaling for video
+  socket.on('webrtc-offer', data => {
+    if(users[data.to]) io.to(users[data.to].socketId).emit('webrtc-offer', {from: socket.number, sdp: data.sdp});
+  });
+  socket.on('webrtc-answer', data => {
+    if(users[data.to]) io.to(users[data.to].socketId).emit('webrtc-answer', {from: socket.number, sdp: data.sdp});
+  });
+  socket.on('webrtc-candidate', data => {
+    if(users[data.to]) io.to(users[data.to].socketId).emit('webrtc-candidate', {from: socket.number, candidate: data.candidate});
+  });
+
   socket.on('call', data => {
-    if(users[data.to]) io.to(users[data.to]).emit('incomingCall', {from: socket.number});
+    if(users[data.to]) io.to(users[data.to].socketId).emit('incomingCall', {from: socket.number});
   });
-
   socket.on('answerCall', data => {
-    if(users[data.to]) io.to(users[data.to]).emit('callAnswered', {from: socket.number});
+    if(users[data.to]) io.to(users[data.to].socketId).emit('callAnswered', {from: socket.number});
   });
-
   socket.on('declineCall', data => {
-    if(users[data.to]) io.to(users[data.to]).emit('callDeclined', {from: socket.number});
+    if(users[data.to]) io.to(users[data.to].socketId).emit('callDeclined', {from: socket.number});
   });
 
   socket.on('disconnect', () => {
