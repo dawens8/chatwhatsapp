88fc9f75ef39a32427bf7f105942da46989e8275
dawens8 <mydawens8@gmail.com>
2025-09-16 22:02:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,93 +2,50 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const multer = require('multer');
-const path = require('path');
 
-app.use(express.static('public'));
-app.use(express.json({limit:'50mb'}));
+app.use(express.static('public')); // put your index.html in 'public' folder
 
-const upload = multer({ dest: 'uploads/' });
+const allowedNumbers = ['13058962443','18573917861'];
+let messages = {}; // store messages per number
+let voiceMessages = {};
 
-let users = {}; // {number:{socketId, photo, video}}
-let messages = {}; // {number:[{from,text,time,type}]}
+io.on('connection', socket => {
+    let currentNumber = null;
 
-// Routes
-app.get('/', (req,res)=>res.sendFile(__dirname+'/public/index.html'));
-app.get('/dashboard', (req,res)=>res.sendFile(__dirname+'/public/dashboard.html'));
-
-// Upload photo
-app.post('/uploadPhoto', upload.single('photo'), (req,res)=>{
-    const {number} = req.body;
-    if(!users[number]) users[number]={};
-    users[number].photo = '/'+req.file.path;
-    io.emit('photoUpdated', {number, photo:users[number].photo});
-    res.send({success:true});
-});
-
-// Upload video (status)
-app.post('/uploadVideo', upload.single('video'), (req,res)=>{
-    const {number} = req.body;
-    if(!users[number]) users[number]={};
-    users[number].video = '/'+req.file.path;
-    io.emit('videoUpdated', {number, video:users[number].video});
-    res.send({success:true});
-});
-
-// Socket.IO
-io.on('connection', socket=>{
-    console.log('New connection', socket.id);
-
-    socket.on('join', number=>{
-        if(!users[number]) users[number]={};
-        users[number].socketId = socket.id;
-        socket.number = number;
-        io.emit('statusUpdate', users);
+    socket.on('join', number => {
+        if(!allowedNumbers.includes(number)) return;
+        currentNumber = number;
+        if(!messages[number]) messages[number] = [];
+        if(!voiceMessages[number]) voiceMessages[number] = [];
+        socket.emit('loadMessages', messages[number]);
+        voiceMessages[number].forEach(v => socket.emit('voice', v));
     });
 
-    socket.on('message', data=>{
-        const time = new Date().toLocaleTimeString([], {hour:'2-digit',minute:'2-digit'});
-        const msg = {from: socket.number, to:data.to, text:data.text, time, type:data.type||'text'};
-        if(!messages[data.to]) messages[data.to]=[];
-        messages[data.to].push(msg);
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('message', msg);
-        io.to(socket.id).emit('message', msg);
+    socket.on('message', msg => {
+        const time = new Date().toLocaleTimeString();
+        const fullMsg = {from: currentNumber, text: msg.text, time};
+        messages[msg.to].push(fullMsg);
+        socket.to(msg.to).emit('message', fullMsg);
     });
 
-    socket.on('voice', data=>{
-        const time = new Date().toLocaleTimeString([], {hour:'2-digit',minute:'2-digit'});
-        const msg = {from: socket.number, to:data.to, audio:data.audio, time, type:'voice'};
-        if(!messages[data.to]) messages[data.to]=[];
-        messages[data.to].push(msg);
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('voice', msg);
-        io.to(socket.id).emit('voice', msg);
+    socket.on('voice', msg => {
+        const fullMsg = {from: currentNumber, audio: msg.audio};
+        voiceMessages[msg.to].push(fullMsg);
+        socket.to(msg.to).emit('voice', fullMsg);
     });
 
-    // WebRTC signaling for video
-    socket.on('webrtc-offer', data=>{
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('webrtc-offer',{from:socket.number,sdp:data.sdp});
-    });
-    socket.on('webrtc-answer', data=>{
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('webrtc-answer',{from:socket.number,sdp:data.sdp});
-    });
-    socket.on('webrtc-candidate', data=>{
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('webrtc-candidate',{from:socket.number,candidate:data.candidate});
+    socket.on('call', data => {
+        socket.to(data.to).emit('incomingCall', {from: currentNumber});
     });
 
-    socket.on('call', data=>{
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('incomingCall',{from:socket.number});
-    });
-    socket.on('answerCall', data=>{
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('callAnswered',{from:socket.number});
-    });
-    socket.on('declineCall', data=>{
-        if(users[data.to] && users[data.to].socketId) io.to(users[data.to].socketId).emit('callDeclined',{from:socket.number});
+    socket.on('answerCall', data => {
+        socket.to(data.to).emit('callAnswered', {from: currentNumber});
     });
 
-    socket.on('disconnect', ()=>{
-        if(socket.number) delete users[socket.number].socketId;
-        io.emit('statusUpdate', users);
+    socket.on('declineCall', data => {
+        socket.to(data.to).emit('callDeclined', {from: currentNumber});
     });
 });
 
-http.listen(3000, ()=>console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
