64b6510775a4f3a0c0241a0554c3ebbb5e2cae8b
dawens8 <mydawens8@gmail.com>
2025-09-21 06:37:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,117 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path');
+const server = http.createServer(app);
+const io = new Server(server);
 
-// Serve static files from 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files (frontend)
+app.use(express.static(path.join(__dirname, "public")));
 
-// Serve index.html at root
-app.get('/', (req,res)=>{
-  res.sendFile(path.join(__dirname, 'public','index.html'));
-});
+let users = {};       // { number: socket.id }
+let reports = {};     // { number: count }
+let blocked = {};     // { blocker: [blockedNumbers] }
 
-const messages = [];
-let callState = {};
+// --- SOCKET.IO HANDLERS ---
+io.on("connection", (socket) => {
+  console.log("New connection:", socket.id);
 
-io.on('connection', socket=>{
-  console.log('a user connected');
-
-  socket.on('join', number=>{
+  // Join user
+  socket.on("join", (number) => {
+    users[number] = socket.id;
     socket.number = number;
-    console.log(number,'joined');
+    console.log(`${number} joined.`);
   });
 
-  socket.on('message', msg=>{
-    msg.from = socket.number;
-    msg.time = new Date().toLocaleTimeString();
-    messages.push(msg);
-    io.emit('message', msg);
+  // Send text
+  socket.on("message", (msg) => {
+    if (blocked[msg.to]?.includes(socket.number)) {
+      console.log("Message blocked by recipient.");
+      return;
+    }
+    const target = users[msg.to];
+    if (target) {
+      io.to(target).emit("message", {
+        from: socket.number,
+        text: msg.text,
+        time: new Date().toLocaleTimeString(),
+      });
+    }
   });
 
-  socket.on('voice', msg=>{
-    msg.from = socket.number;
-    io.emit('voice', msg);
+  // Send voice
+  socket.on("voice", (msg) => {
+    const target = users[msg.to];
+    if (target) {
+      io.to(target).emit("voice", {
+        from: socket.number,
+        audio: msg.audio,
+      });
+    }
   });
 
-  socket.on('call', data=>{
-    const to = data.to;
-    callState[to] = {incomingFrom: socket.number, isInCall:false};
-    io.emit('incomingCall', {from: socket.number, to});
+  // Call events
+  socket.on("call", (data) => {
+    const target = users[data.to];
+    if (target) {
+      io.to(target).emit("incomingCall", { from: socket.number });
+    }
   });
 
-  socket.on('answerCall', data=>{
-    const to = data.to;
-    callState[socket.number].isInCall = true;
-    io.emit('callAnswered', {from: socket.number, to});
+  socket.on("answerCall", (data) => {
+    const target = users[data.to];
+    if (target) io.to(target).emit("callAnswered", { from: socket.number });
   });
 
-  socket.on('declineCall', data=>{
-    const to = data.to;
-    io.emit('callDeclined', {from: socket.number, to});
+  socket.on("declineCall", (data) => {
+    const target = users[data.to];
+    if (target) io.to(target).emit("callDeclined", { from: socket.number });
   });
 
-  socket.on('disconnect', ()=>{
-    console.log(socket.number,'disconnected');
+  // Block / Unblock
+  socket.on("block", (num) => {
+    if (!blocked[socket.number]) blocked[socket.number] = [];
+    if (!blocked[socket.number].includes(num)) {
+      blocked[socket.number].push(num);
+    }
+    socket.emit("blockedList", blocked[socket.number]);
+  });
+
+  socket.on("unblock", (num) => {
+    if (blocked[socket.number]) {
+      blocked[socket.number] = blocked[socket.number].filter((n) => n !== num);
+    }
+    socket.emit("blockedList", blocked[socket.number]);
+  });
+
+  // Report system
+  socket.on("report", (num) => {
+    if (!reports[num]) reports[num] = 0;
+    reports[num]++;
+
+    if (reports[num] >= 7) {
+      const target = users[num];
+      if (target) {
+        io.to(target).emit("banned", {
+          reason: "Too many reports",
+          duration: "24h",
+        });
+        io.sockets.sockets.get(target)?.disconnect();
+      }
+    }
+  });
+
+  // Disconnect
+  socket.on("disconnect", () => {
+    console.log(`${socket.number} disconnected`);
+    delete users[socket.number];
   });
 });
 
-http.listen(3000, ()=>console.log('Server running on http://localhost:3000'));
+// Render use process.env.PORT
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
